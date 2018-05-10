cdb8446e824154915e0d447b29049884c2f2a303
ikrokhmaliuk <38694904+ikrokhmaliuk@users.noreply.github.com>
2018-05-10 20:49:47 +0300

    add classes

---
+++ src/MyCustomFileParser.java
@@ -0,0 +1,12 @@
+public class MyCustomFileParser {
+
+    public boolean findWord(String word) { }
+
+    public boolean replaceWord(String word) { }
+
+    public boolean deleteWord(String word) {  }
+
+    public boolean addWord(String word){  }
+
+
+}
