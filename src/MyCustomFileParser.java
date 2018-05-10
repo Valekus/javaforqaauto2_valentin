ba683d3bb33b5433d44e39101230580d6c76598a
Valentin Usatiy <Valekus777@gmail.com>
2018-05-10 20:50:25 +0300

    add interface

---
+++ src/MyCustomFileParser.java
@@ -0,0 +1,7 @@
+public interface MyCustomFileParser {
+    public boolean findWord(String word);
+    public boolean replaceWord(String word);
+    public boolean deleteWord(String word);
+    public boolean addWord(String word);
+
+}
