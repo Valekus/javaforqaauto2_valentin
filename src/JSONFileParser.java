132c6c3d34950f9f4e0c5fbe948a38b46eef34ce
ikrokhmaliuk <38694904+ikrokhmaliuk@users.noreply.github.com>
2018-05-10 21:20:11 +0300

    add json

---
+++ src/JSONFileParser.java
@@ -0,0 +1,21 @@
+public class JSONFileParser implements MyCustomFileParserInterface {
+    @java.lang.Override
+    public boolean findWord(String word) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean replaceWord(String word) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean deleteWord(String word) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean addWord(String word) {
+        return false;
+    }
+}
