859ab77a50434b7d604430b7e1f6b7163c8da3d8
Valentin Usatiy <Valekus777@gmail.com>
2018-05-13 11:47:57 +0300

    one more commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
 
+        MyCustomFileParserInterface myP = null;
+        if (args[0].equals("JSON")){
+            myP = new JSONFileParser();
+
+        }else if(args[0].equals("XML")){
+            myP = new XMLFileParser();
+        }
+        myP.addWord("New word");
+        myP.findWord("New");
+
+    }
 }
